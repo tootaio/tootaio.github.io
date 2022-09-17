f8054571f22b6b4a6c07cc80732596e6c09e97ea
hang333 <jonathan.goh333@gmail.com>
2022-09-17 21:34:45 +0800

    Integrate with webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  module: {
+    rules: [
+      { test: /\.txt$/, use: "raw-loader" },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.ejs" })],
+};
